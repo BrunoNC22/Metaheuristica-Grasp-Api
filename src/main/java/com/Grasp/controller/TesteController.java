2343102253b0271a3d1b75a37a15dfdf4ac08f03
BrunoNC22 <thebegone2@gmail.com>
2023-08-17 07:37:18 -0300

    Feat: initial commit

---
+++ src/main/java/com/Grasp/controller/TesteController.java
@@ -0,0 +1,33 @@
+package com.Grasp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Grasp.entity.Graph;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+	
+	@GetMapping
+	public Graph test() {
+		Graph test = new Graph("testeGraph");
+		
+		return test;
+	}
+	
+	@PostMapping
+	public Graph createGraph(@RequestBody Graph graph) {
+		Graph newGraph = new Graph(graph.getNome());
+		
+		return newGraph;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/Grasp/entity/Graph.java
@@ -0,0 +1,21 @@
+package com.Grasp.entity;
+
+public class Graph {
+	
+	private String nome;
+	
+	public Graph(String nome) {
+		this.nome = nome;
+	}
+	
+	public Graph() {}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
